f120e006b786d61e71e7664d5b784857b864253a
Tchamokouin Tsemo just-donald <tsemojust@gmail.com>
2025-03-08 09:50:22 -0500

    fixing javascript bug

--- src/index.js
+++ src/index.js
@@ -1,39 +1,37 @@
-var one = document.querySelector(".one");
-var two = document.querySelector(".two");
-var three = document.querySelector(".three");
-var four = document.querySelector(".four");
-var five = document.querySelector(".five");
-var rating_container = document.querySelector(".rating_state_container");
-var thank_container = document.querySelector(".thank_you_state_container");
-var current_rating = document.querySelector(".current_rating");
+let one = document.querySelector(".one");
+let two = document.querySelector(".two");
+let three = document.querySelector(".three");
+let four = document.querySelector(".four");
+let five = document.querySelector(".five");
+let rating_container = document.querySelector(".rating_state_container");
+let thank_container = document.querySelector(".thank_you_state_container");
+let current_rating = document.querySelector(".current_rating");
 let rating_button = document.querySelector(".rating_button");
-
-let rating_numbers = document.querySelectorAll(".rating_number");
 
 const toggleActive = (item) => {
   item.classList.toggle("active");
 };
 
-one.addEventListener("click", () => {
-  toggleActive(one);
-  current_rating.innerHTML = one.innerHTML;
-});
-two.addEventListener("click", () => {
-  toggleActive(two);
-  current_rating.innerHTML = two.innerHTML;
-});
-three.addEventListener("click", () => {
-  toggleActive(three);
-  current_rating.innerHTML = three.innerHTML;
-});
-four.addEventListener("click", () => {
-  toggleActive(four);
-  current_rating.innerHTML = four.innerHTML;
-});
-five.addEventListener("click", () => {
-  toggleActive(five);
-  current_rating.innerHTML = five.innerHTML;
-});
+let ratingSelectedNumbers = [one, two, three, four, five];
+
+const checkIfIsActive = (item) => {
+  ratingSelectedNumbers.forEach((elt) => {
+    if (item !== elt && elt.classList.contains("active")) {
+      toggleActive(elt);
+    }
+  });
+};
+
+const selectedNumberHandler = () => {
+  ratingSelectedNumbers.forEach((item) => {
+    item.addEventListener("click", () => {
+      toggleActive(item);
+      checkIfIsActive(item);
+      current_rating.innerHTML = item.innerHTML;
+    });
+  });
+};
+selectedNumberHandler();
 
 rating_button.addEventListener("click", () => {
   rating_container.style.display = "none";
